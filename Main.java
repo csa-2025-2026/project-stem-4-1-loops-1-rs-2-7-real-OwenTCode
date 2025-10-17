2046daf4ad3f42b239620759ea410fa1b3fffbb1
OwenTCode <Oweneight88@gmail.com>
2025-10-17 18:17:40 +0000

    ghfuiahf

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    //Loop that prints the odd numbers between 3 and 25
+
+    int blub = 3;
+
+    while (blub <= 25)
+    {
+      System.out.println(blub);
+      blub = (blub + 2);
+    }
   }
 }
